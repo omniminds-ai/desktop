b76d9623c0e748149d59fe62992a1c8d91bdcd50
Morgan Dean <morgandeanhi@gmail.com>
2025-02-26 17:16:41 -0800

    macos-specific deps

--- src-tauri/src/permissions.rs
+++ src-tauri/src/permissions.rs
@@ -1,34 +1,38 @@
-use core_foundation::base::Boolean;
-use core_graphics::access::ScreenCaptureAccess;
+#[cfg(target_os = "macos")]
+mod macos_permissions {
+    use core_foundation::base::Boolean;
+    use core_graphics::access::ScreenCaptureAccess;
+    use macos_accessibility_client::accessibility::application_is_trusted_with_prompt;
 
-// External ApplicationServices declarations
-#[link(name = "ApplicationServices", kind = "framework")]
-extern "C" {
-    fn AXIsProcessTrusted() -> Boolean;
-}
+    // External ApplicationServices declarations
+    #[link(name = "ApplicationServices", kind = "framework")]
+    extern "C" {
+        fn AXIsProcessTrusted() -> Boolean;
+    }
 
-#[tauri::command]
-pub fn has_ax_perms() -> bool {
-    unsafe {
-        let result = AXIsProcessTrusted() != 0;
-        result
+    #[tauri::command]
+    pub fn has_ax_perms() -> bool {
+        unsafe {
+            let result = AXIsProcessTrusted() != 0;
+            result
+        }
+    }
+
+    #[tauri::command]
+    pub fn request_ax_perms() {
+        application_is_trusted_with_prompt();
+    }
+
+    #[tauri::command]
+    pub fn has_record_perms() -> bool {
+        return ScreenCaptureAccess.preflight();
+    }
+
+    #[tauri::command]
+    pub fn request_record_perms() {
+        ScreenCaptureAccess.request();
     }
 }
 
-#[tauri::command]
-pub fn request_ax_perms() {
-    #[cfg(target_os = "macos")]
-    {
-        macos_accessibility_client::accessibility::application_is_trusted_with_prompt();
-    }
-}
-
-#[tauri::command]
-pub fn has_record_perms() -> bool {
-    return ScreenCaptureAccess.preflight();
-}
-
-#[tauri::command]
-pub fn request_record_perms() {
-    ScreenCaptureAccess.request();
-}
+#[cfg(target_os = "macos")]
+pub use macos_permissions::*;
